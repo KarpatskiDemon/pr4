2765d9869f19639de7f3b44216173f193031b29b
KarpatskiDemon <kuzevjeki4@gmail.com>
2024-10-14 23:46:06 +0300

    Add files via upload

---
+++ n2/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+//text
+        System.out.println("Введіть текст:");
+        String txt = scanner.nextLine();
+//kilkrech
+//while
+        int a = 0;
+        int b = 0;
+
+        while (b < txt.length()) {
+            char cr = txt.charAt(b);
+            if (cr == '.' || cr == '!' || cr == '?') {
+                a++;
+            }
+            b++;
+        }
+
+        System.out.println("Кількість речень (while): " + a);
+//do-while
+        int c = 0;
+        b = 0;
+
+        do {
+            char cr = txt.charAt(b);
+            if (cr == '.' || cr == '!' || cr == '?') {
+                c++;
+            }
+            b++;
+        } while (b < txt.length());
+
+        System.out.println("Кількість речень (do-while): " + c);
+
+        // Закриття сканера
+        scanner.close();
+    }
+}
